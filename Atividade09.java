f53e7f422843bf77ff3aa52c1ad33da1978256b5
Syllas <syllasbraga2@gmail.com>
2024-08-18 15:29:55 -0300

    Exercicio 09

--- Atividade09.java
+++ Atividade09.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Atividade09 {
     public static void main(String[] args){
-
+        Scanner sc = new Scanner(System.in);
+        Double raio = sc.nextDouble();
+        Double area = Math.PI * Math.pow(raio, 2);
+        System.out.println("Valor da área: "+ Math.round(area));
     }
 }
