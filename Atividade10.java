601fe6c1782ef6000b399d5420cac42c6b4ac376
Syllas <syllasbraga2@gmail.com>
2024-08-18 16:01:57 -0300

    Exercicio 10

--- Atividade10.java
+++ Atividade10.java
@@ -1,5 +1,29 @@
-public class Atividade10 {
-    public static void main(String[] args){
+import java.util.Scanner;
 
+public class Atividade10 {
+    public static void main(String[] args) {
+        int[] cont = {0, 0, 0};
+        Scanner sc = new Scanner(System.in);
+        String cadeiaCaracteres = sc.nextLine();
+        for (int i = 0; i < cadeiaCaracteres.length(); i++) {
+            if (validaVogal(cadeiaCaracteres.charAt(i))) {
+                cont[0]++;
+            } else if (Character.isWhitespace(cadeiaCaracteres.charAt(i))) {
+                cont[1]++;
+            } else if (Character.isLetter(cadeiaCaracteres.charAt(i))) {
+                cont[2]++;
+            }
+        }
+        System.out.println("Vogais: "+cont[0]+"\nEspaços em branco: "+cont[1]+"\nConsoantes: "+cont[2]);
+    }
+
+    private static boolean validaVogal(char caracter) {
+        String[] vogais = {"a", "e", "i", "o", "u", "y"};
+        for (int j = 0; j < vogais.length; j++) {
+            if (caracter == vogais[j].charAt(0)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
