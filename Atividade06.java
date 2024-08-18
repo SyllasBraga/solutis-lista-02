ca44d7c47c38272c17a12c528280fb30b3c7d17e
Syllas <syllasbraga2@gmail.com>
2024-08-18 14:57:22 -0300

    Exercicio 06

--- Atividade06.java
+++ Atividade06.java
@@ -1,5 +1,14 @@
 public class Atividade06 {
     public static void main(String[] args){
-
+        for (int i =15; i<=30; i++){
+            for (int j=15; j<=30; j++){
+                if (i%2 != 0 && j%2 != 0){
+                    int produtoInt = i*j;
+                    float produtoFloat = i*j;
+                    System.out.println("Inteiro: " +i + " * " + j + " = "+produtoInt);
+                    System.out.println("Float:   " +i + " * " + j + " = "+produtoFloat);
+                }
+            }
+        }
     }
 }
