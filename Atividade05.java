5071a0537ed192d64610fccdeacd3cd9df33280b
Syllas <syllasbraga2@gmail.com>
2024-08-18 14:48:54 -0300

    Exercicio 05

--- Atividade05.java
+++ Atividade05.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Atividade05 {
     public static void main(String[] args){
-
+        String[] dias = {"Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira",
+                "Sábado"};
+        Scanner sc = new Scanner(System.in);
+        int dia = 0;
+        do{
+            dia = sc.nextInt();
+        }while(dia>7 || dia<1);
+        System.out.println(dias[dia-1]);
     }
 }
