76be6e1e88a5bf4f537c8d72961ac4851b4f4e6c
Syllas <syllasbraga2@gmail.com>
2024-08-18 16:09:54 -0300

    Exercicio 11

--- Atividade11.java
+++ Atividade11.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Atividade11 {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
 
+        String palavra1 = sc.nextLine();
+        String palavra2 = sc.nextLine();
+
+        if (palavra1.compareTo(palavra2) < 0) {
+            System.out.println("Ordem alfabética: " + palavra1 + ", " + palavra2);
+        } else {
+            System.out.println("Ordem alfabética: " + palavra2 + ", " + palavra1);
+        }
+
+        if (palavra1.length() > palavra2.length()) {
+            System.out.println("A palavra com o maior número de caracteres é: " + palavra1);
+        } else if (palavra1.length() < palavra2.length()) {
+            System.out.println("A palavra com o maior número de caracteres é: " + palavra2);
+        } else {
+            System.out.println("Ambas as palavras têm o mesmo número de caracteres.");
+        }
     }
 }
