17bc0dd1ecd98c1c3bf7d2d27365125e292181df
Syllas <syllasbraga2@gmail.com>
2024-08-18 14:43:28 -0300

    Exercicio 04

--- Atividade04.java
+++ Atividade04.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Atividade04 {
     public static void main(String[] args){
-
+        Scanner sc = new Scanner(System.in);
+        double milhas = sc.nextDouble();
+        double milhasParaQuilometros = 1.609;
+        double quilometros = milhas * milhasParaQuilometros;
+        System.out.println("Valor de milhas para quilômetros: "+ quilometros +"kms");
     }
 }
