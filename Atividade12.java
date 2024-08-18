726ed82eb1175bc64a6b8a8cc9103d0577095c4e
Syllas <syllasbraga2@gmail.com>
2024-08-18 16:37:13 -0300

    Exercicio 12

--- Atividade12.java
+++ Atividade12.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Atividade12 {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String condicao = "";
+        int quilometragemTotal = 0;
+        int litrosCombustivelTotal = 0;
+        while(!condicao.equals("pare")) {
+            System.out.print("Digite os kms percorridos: ");
+            int quilometros = sc.nextInt();
+            sc.nextLine();
 
+            System.out.print("Digite os listros de combustível consumido: ");
+            int litrosCombustivel = sc.nextInt();
+            sc.nextLine();
+
+            float mediaConsumo = (float) quilometros / litrosCombustivel;
+            quilometragemTotal += quilometros;
+            litrosCombustivelTotal += litrosCombustivel;
+
+            System.out.println("Consumo médio: "+ mediaConsumo+ "km/l\nTotal de quilômetros rodados: "+
+                    quilometragemTotal + "kms\nTotal de combustível consumido: "+ litrosCombustivelTotal + "l");
+
+            System.out.println("Digite 'pare' para sair ou pressione 'enter' para continuar");
+            condicao = sc.nextLine();
+        }
     }
 }
