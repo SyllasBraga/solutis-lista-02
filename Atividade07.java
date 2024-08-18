30cbdbae1f2e3d8749de9d64716ddfd242daa212
Syllas <syllasbraga2@gmail.com>
2024-08-18 15:06:21 -0300

    Exercicio 07

--- Atividade07.java
+++ Atividade07.java
@@ -1,5 +1,12 @@
 public class Atividade07 {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i%3 == 0) {
+                int resultadoInt = i / 2;
+                float resultadoFloat = i / 2.0f;
+                System.out.println("Inteiro: " + i + " / " + 2 + " = " + resultadoInt);
+                System.out.println("Float:   " + i + " / " + 2 + " = " + resultadoFloat);
+            }
+        }
     }
 }
